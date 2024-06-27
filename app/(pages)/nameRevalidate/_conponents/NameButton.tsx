85215749e8773927f787c961e110b4cbfc55e5c3
aptakqmf12 <aptakqmf12@gmail.com>
2024-06-27 22:56:16 +0900

    error fix

--- app/(pages)/nameRevalidate/_conponents/NameButton.tsx
+++ app/(pages)/nameRevalidate/_conponents/NameButton.tsx
@@ -2,20 +2,16 @@ import { revalidateTag } from "next/cache";
 import React from "react";
 
 export default function NameButton({ newName }: { newName: string }) {
-  const handleButtonClick = async () => {
-    const res = await fetch("http://localhost:3000/api/test", {
-      method: "post",
-      body: JSON.stringify({ newName }),
-    });
+  // const handleButtonClick = async () => {
+  //   const res = await fetch("http://localhost:3000/api/test", {
+  //     method: "post",
+  //     body: JSON.stringify({ newName }),
+  //   });
 
-    const json = await res.json();
+  //   const json = await res.json();
 
-    revalidateTag("myName");
-  };
+  //   revalidateTag("myName");
+  // };
 
-  return (
-    <div>
-      <button onClick={handleButtonClick}>post!</button>;
-    </div>
-  );
+  return <div>{/* <button onClick={handleButtonClick}>post!</button>; */}</div>;
 }
--- app/(pages)/nameRevalidate/_conponents/NameTag.tsx
+++ app/(pages)/nameRevalidate/_conponents/NameTag.tsx
@@ -1,10 +1,10 @@
 export default async function NameTag() {
-  const res = await fetch(`http://localhost:3000/api/test`, {
-    next: {
-      tags: ["myName"],
-    },
-  });
-  const { name } = await res.json();
+  // const res = await fetch(`http://localhost:3000/api/test`, {
+  //   next: {
+  //     tags: ["myName"],
+  //   },
+  // });
+  // const { name } = await res.json();
 
-  return <div>nameTag : {name}</div>;
+  return <div>nameTag :</div>;
 }
